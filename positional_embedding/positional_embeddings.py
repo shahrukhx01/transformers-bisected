e705638b271029eef792ecacc05e3e3309cf6b20
shahrukhx01 <sk28671@gmail.com>
2021-04-13 15:23:40 +0000

    add poem class

--- positional_embedding/positional_embeddings.py
+++ positional_embedding/positional_embeddings.py
@@ -1,10 +1,24 @@
 import torch
 import nltk
-nltk.download('punkt') 
+#nltk.download('punkt')  ## uncomment if running for the first time
 
 class PositionalEmbedding:
-    def __init__(self):
+    def __init__(self, data_path):
+        pass
+    
+    def get_vanilla_word_emb(self, word):
+        pass
+    
+    def get_vanilla_pos_emb(self, position):
+        pass
+    
+    def get_positional_embedding(self, word_emb, positional_emb):
         pass
 
+    @staticmethod
+    def compute_similarity(embedding1, embedding2):
+        pass
 
-
+if __name__ == "__main__":
+    PositionalEmbedding(data_path)
+    print('hello')
