633ab3040b36d1892b6813fb2a948e4e3ff5bda7
shahrukhx01 <sk28671@gmail.com>
2021-04-14 16:26:22 +0000

    add self attention starter

--- multihead_attention/self_attention.py
+++ multihead_attention/self_attention.py
@@ -17,13 +17,36 @@ class SelfAttention(nn.Module):
         self.fc = nn.Linear(self.head_dimension * self.heads, self.embed_size)
     
 
-    def forward(self, query, keys, values, mask):
+    def forward(self, queries, keys, values, mask):
         N = query.shape[0]
-        key_len, query_len, value_len = keys.shape[1], query.shape[1], values.shape[1]
+        key_len, query_len, value_len = keys.shape[1], queries.shape[1], values.shape[1]
 
         ## split embedding into self.heads pieces
         values = values.reshape(N, value_len, self.heads, self.head_dimension) 
         keys = keys.reshape(N, key_len, self.heads, self.head_dimension) 
         query = query.reshape(N, query_len, self.heads, self.head_dimension) 
 
-        ## 
+       ## get attention based keys, queries and values
+       keys = self.keys(keys)
+       values = self.values(values)
+       queries = self.queries(queries)
+
+      ## now perform Q.K step from the paper
+      ## queries shape: (N, query_len, heads, head_dimension)
+      ## keys shape: (N, key_len, heads, head_dimension)
+      ## energy shape: (N, heads, query_len, key_len)
+
+      energy = torch.einsum("nqhd, nkhq-> nhqk")
+
+      if mask is not None:
+          energy = energy.masked_fill(mask==0, float("-1e20"))
+
+     ## attention = softmax(QK/sqrt(dk))
+     attention = torch.softmax(energy / (self.embed_size**(1/2)))
+    
+     ## get final attention
+     ## attention shape: (N, heads, query_len, key_len)
+     ## values shape: (N, value_len, heads, head_dimension)
+     
+    
+     
